cd5f71c08e70b0c5a2bb27003fb21ebd85ff3e93
Mitchell Stowman <mitchellstowman@gmail.com>
2023-02-23 21:54:42 -0500

    Established Routes and Navigation - Navigation Bar implemented and styled - Pathways set using react-router

--- src/App.js
+++ src/App.js
@@ -1,9 +1,23 @@
+import { Routes, Route } from 'react-router-dom';
+
 import './App.css';
+import Navigation from './routes/Navigation/Navigation.comp';
+import Home from './routes/Home/home.comp';
+import Portfolio from './routes/Portfolio/portfolio.comps';
+import AboutUs from './routes/About/about.comp';
+import ContactUs from './routes/Contact/contact.comp';
 
 function App() {
   return (
-    <div className="App">
-      <h1>Arch Studio</h1>
+    <div>
+      <Routes>
+        <Route path='/' element={<Navigation />}>
+          <Route index='true' element={<Home />} />
+          <Route path='/portfolio' element={<Portfolio />} />
+          <Route path='/about' element={<AboutUs />} />
+          <Route path='/contact' element={<ContactUs />} />
+        </Route>
+      </Routes>
     </div>
   );
 }
