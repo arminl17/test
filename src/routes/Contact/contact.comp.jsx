1840db37f157967c23d0c6bd9d7c22016dbc2b0a
Mitchell Stowman <mitchellstowman@gmail.com>
2023-02-27 20:08:22 -0500

    Finished form validation

--- src/routes/Contact/contact.comp.jsx
+++ src/routes/Contact/contact.comp.jsx
@@ -1,3 +1,5 @@
+import { useState } from 'react';
+
 // Styles
 import '../Contact/contact.styles.scss';
 
@@ -10,7 +12,45 @@ import Map from '../../assets/contact/desktop/image-map.png';
 import Arrow from '../../assets/icons/icon-arrow.svg'
 ;
 export default function Contact() {
-    return(
+
+  const [formError, setFormError] = useState({
+    name: '',
+    email: '',
+    message: ''
+  });
+  
+  const submitForm = (event) => {
+      event.preventDefault();
+      const name = event.target.name.value;
+      const email = event.target.email.value;
+      const message = event.target.message.value;
+    }
+
+  const nameHandler = (e) => {
+    if (e.target.value == 0) {
+      setFormError({...formError, name: 'Input field cannot be left blank.'})
+    } else {
+      setFormError({...formError, name: ''})
+    }
+  }
+
+  const emailHandler = (e) => {
+    if (!e.target.value.includes('@' && '.')) {
+      setFormError({...formError, email: 'Email format is incorrect.'})
+    } else {
+      setFormError({...formError, email: ''})
+    } 
+  }
+
+  const messageHandler = (e) => {
+    if (e.target.value == 0) {
+      setFormError({...formError, message: 'Message cannot be left blank.'})
+    } else {
+      setFormError({...formError, email:''})
+    }
+  }
+  
+  return(
       <div>
         {/* Vertical Text */}
         <div className='vert-container'>
@@ -20,6 +60,7 @@ export default function Contact() {
 
         <div className='page-container'>
         
+
           <div className='contact-hero-container'>
             <img src={HeroImage} alt='' />
             <h1>Contact</h1>
@@ -70,11 +111,40 @@ export default function Contact() {
           <div className='form-section-container'>
             <h3>Connect<br></br>with us</h3>
 
-            <form>
-              <input placeholder='Name'></input>
-              <input placeholder='Email'></input>
-              <input className='message' placeholder='Message'></input>
-              <button>
+            <form onSubmit={submitForm}>
+              <div className='input-container'>
+                <label>{formError.name}</label>
+                <input 
+                onChange={nameHandler}
+                type='text'
+                name='name'
+                id='name' 
+                placeholder='Name' 
+                required
+                ></input>
+              </div>
+              
+              <div className='input-container'>
+                <label>{formError.email}</label>
+                <input 
+                onChange={emailHandler}
+                type='text' 
+                name='email' 
+                placeholder='Email' 
+                required
+                ></input>
+              </div>
+              
+              <input 
+                onChange={messageHandler}
+                type='text' 
+                name='message' 
+                className='message' 
+                placeholder='Message'
+                required>
+              </input>
+
+              <button type='submit'>
                 <img src={Arrow} />
               </button>
             </form>
@@ -83,9 +153,25 @@ export default function Contact() {
 
         </div> {/* End of Page Container*/}
 
-
-
       <Footer />
       </div>   
     )
   }
+
+
+
+  /*
+  TO-DO:
+- Receive an error message when the contact form is submitted if:
+  - The `Name`, `Email` or `Message` fields are empty should show "Can't be empty"
+  - The `Email` is not formatted correctly should show "Please use a valid email address"
+
+- **Bonus**: View actual locations on the contact page map. The addresses we have on 
+the design are fictional, so you'll need to add real addresses for this bonus task.
+
+⚠️ **IMPORTANT** ⚠️: If you choose to do the bonus part of the challenge and use a mapping API,
+ we recommend using [LeafletJS](https://leafletjs.com/). It's free to use and doesn't require an 
+ API Key. If you decide to use another API, like Google Maps or Mapbox, be sure to secure your 
+ API Key. Here are guides for both Google Maps and Mapbox, be sure to read through them thoroughly:
+
+  */
